9685615117bca34e394e5c7baf350414650d3309
SQDNK <hsliu@andrew.cmu.edu>
2023-06-06 21:43:57 -0400

    added fine tuning and using curr recs as seeds

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ app.get('/login', function(req, res) {
 
   // your application requests authorization
   let scope = 'user-read-private user-read-email playlist-modify-public \
-               playlist-modify-private';
+               playlist-modify-private playlist-read-private';
   res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
@@ -137,10 +137,78 @@ app.get('/callback', async function(req, res) {
     // should have no error here
     console.log(dataG);*/
 
+    let playlist_id = '3BHHjHO7e1rV2yzCs54bnV';
+
+    // use (all) tracks in playlist as seed tracks to get recs
+    let urlT = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
+    let fetchParamsObjT = {method: 'GET',
+                           headers: {'Authorization': 'Bearer ' + access_token},
+                           json: true};
+    const dataT = await fetchAndCatchError(res, urlT, fetchParamsObjT);
+    let seedTracksString = "";
+    let count = 0; 
+    // adds the first 5 tracks of playlist since spotify's seed limit is 5.
+    for(const trackItem of dataT.items) {
+      seedTracksString += `${trackItem.track.id},`;
+      count++;
+      if (count >= 5) {
+        break;
+      }
+    };   
+    // remove last comma
+    seedTracksString = seedTracksString.slice(0, seedTracksString.length-1);
+
+    // have the option of fine tuning recs from seeds
+    // **TODO: without seeds? 
+    // **TODO: show audio features to user
+    
+    // **TODO: currently uses string with limit of 5 tracks. allow users to put
+    // in more
+    let paramsF = new URLSearchParams();
+    paramsF.append("ids", seedTracksString);
+    let urlF = `https://api.spotify.com/v1/audio-features?${paramsF.toString()}`;
+    let fetchParamsObjF = {method: 'GET',
+                           headers: {'Authorization': 'Bearer ' + access_token},
+                           json: true};
+    const dataF = await fetchAndCatchError(res, urlF, fetchParamsObjF);  
+    
+    let featureMap = new Map([["acousticness", 0], ["danceability",0], 
+      ["energy", 0], ["loudness", 0],["speechiness", 0], ["tempo", 0], ["valence", 0]]); 
+    const numOfTracks = dataF.audio_features.length;
+    for (const track of dataF.audio_features) {
+      for (const [key, value] of featureMap) {
+        // to find best recs, average the params (is this the best way?)
+        // dynamically update avg by doing (avg * length + newElem) / length
+        featureMap.set(key, (value*numOfTracks + track[key]) / numOfTracks  );
+      }
+    }
+
     // call spotify web api to get recs
     let paramsR = new URLSearchParams();
-    paramsR.append("limit", '10');
-    paramsR.append("seed_tracks", '7FbrGaHYVDmfr7KoLIZnQ7,1UglC0beXmI87ubdZgTXbr')
+    paramsR.append("limit", 20);
+    paramsR.append("seed_tracks", seedTracksString);
+    for (const [key, value] of featureMap) {
+      paramsR.append("target_"+key, value);
+    }
+
+    // **TODO: what if there are no seeds and only params for fine tuning?
+    // **TODO: fine tuning will repeat tracks
+    /*
+    choose features that can be input to rec api and output from track feature api
+    also lets you use more than 5 tracks
+    url for full audio analysis possible 
+    min, max, target acousticness (maybe just target?)
+    danceability
+    energy
+    instrumentalness (excluded for now)
+    key (excluded for now)
+    liveness (excluded)
+    loudness
+    mode (excluded for now) (major or minor = 1 or 0)
+    speechiness (excluded if only finding recs for tracks)
+    tempo
+    time_signature
+    valence (musical positiveness) 
     /*
     let seedArtists = document.getElementById("seed-artists").value; 
     let seedGenres = document.getElementById("seed-genres").value;
@@ -165,10 +233,9 @@ app.get('/callback', async function(req, res) {
     let uriArray = [];
     let posValue = 0;
     dataR.tracks.forEach((track) => {
-      uriArray.push(`spotify:track:${track.id}`);
+      uriArray.push(`${track.uri}`);
     });
     let bodyA = {"uris": uriArray, "position": posValue};
-    let playlist_id = '1rjnXiZAt7ZNQMF8fWADYv';
     let urlA = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
     let fetchParamsObjA = {method: 'POST',
                            headers: {'Authorization': 'Bearer ' + access_token, 
